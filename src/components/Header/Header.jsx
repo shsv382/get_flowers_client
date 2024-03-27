de2db31fa20fbaf710a98355c4d6d709f8e9aeb5
Sergey Shabanov <shsv382@gmail.com>
2024-03-27 07:13:51 +0300

    Added some styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,27 @@
 import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
+import { Container } from "@mui/material";
 
 export const Header = (props) => {
 	return <header {...props}>
-		<div className={styles.container}>
-			<div className={styles.logoContainer}>
-				<Link className={styles.option} to='/'>LOGO</Link>
+		<Container>
+			<div className={styles.container}>
+				<div className={styles.logoContainer}>
+					<Link to='/' className={styles.logoLink}>
+						<img src='logo192.png' className={styles.logo} />
+						<span className={styles.logoText}>
+							Get Flowers
+						</span>
+					</Link>
+				</div>
+				<nav>
+					<ul className={styles.optionsContainer}>
+						<li className={styles.optionsLink}><Link className={styles.option} to='/admin'>Admin</Link></li>
+						<li className={styles.optionsLink}><Link className={styles.option} to='/shop'>Shop</Link></li>
+						<li className={styles.optionsLink}><Link className={styles.option} to='/cart'>Cart</Link></li>
+					</ul>
+				</nav>
 			</div>
-		</div>
-		<nav>
-			<ul className={styles.optionsContainer}>
-				<li className={styles.optionsLink}><Link className={styles.option} to='/admin'>Admin</Link></li>
-				<li className={styles.optionsLink}><Link className={styles.option} to='/shop'>Shop</Link></li>
-				<li className={styles.optionsLink}><Link className={styles.option} to='/cart'>Cart</Link></li>
-			</ul>
-		</nav>
+		</Container>
 	</header>;
 };
